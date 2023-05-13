023c7f9fc9a2469dc8c6951710e86bcf64d55a92
aaronchewbani <aaronchewbani@gmail.com>
2023-05-13 14:58:43 -0700

    fix prices

--- src/scripts/prices.js
+++ src/scripts/prices.js
@@ -1,8 +1,15 @@
+const USDECOPrice = .015
 export const getSwapAmount = async function (token1, amt, token2) {
   try {
     const swapInfo = await (await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${token1.name}&tsyms=${token2.name}`)).json()
-    console.log(swapInfo)
-    const amount = swapInfo[token2.name] * amt;
+    const ETHUSD = await toUSD("ETH", amt)
+    let amount = swapInfo[token2.name] * amt;
+    if(token2.name.toLowerCase() =="eco"){
+      amount = ETHUSD/USDECOPrice
+    }
+    else if(token1.name.toLowerCase() =="eco"){
+      amount = USDECOPrice/ETHUSD
+    }
     return amount.toFixed(2)
   } catch(e){
     console.log(e)
